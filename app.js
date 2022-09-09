0aa8fef6ef424019d316e609d975650ab1525dfb
DimasPramantya <igkdimas@gmail.com>
2022-09-09 20:58:50 +0700

    install sequelize and add migration users

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
+const userRouter = require('./app/user/route')
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/user", userRouter);
 
 module.exports = app;
