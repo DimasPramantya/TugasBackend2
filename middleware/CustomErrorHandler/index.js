b069ebaa82ed3ef8468c384c59bb31978b650cd3
DimasPramantya <101824833+DimasPramantya@users.noreply.github.com>
2022-09-24 12:01:40 +0700

    Delete middleware directory

--- middleware/CustomErrorHandler/index.js
+++
@@ -1,8 +0,0 @@
-function CustomErrorHandler(err,req,res,next){
-    res.status(400).json({
-        status: "error",
-        message: err.message,
-    })
-}
-
-module.exports = CustomErrorHandler;
